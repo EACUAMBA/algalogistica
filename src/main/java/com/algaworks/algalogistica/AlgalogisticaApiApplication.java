f01ac41ba995c4bdcb99a406f83cb143ae2d5c96
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-08-13 20:51:10 +0200

    Criando o projecto e preparando o ambiente.

---
+++ src/main/java/com/algaworks/algalogistica/AlgalogisticaApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalogistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgalogisticaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgalogisticaApiApplication.class, args);
+	}
+
+}
